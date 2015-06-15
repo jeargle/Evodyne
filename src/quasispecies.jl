080b2fa98aad5e7b53ab38cc9d376ed85d6b7b02
John Eargle <jeargle@gmail.com>
2015-06-14 23:13:10 -0500

    added Module Quasispecies and moved test

--- src/quasispecies.jl
+++ src/quasispecies.jl
@@ -2,6 +2,10 @@
 # 2015
 # quasispecies
 
+module Quasispecies
+
+export printsummary, quasispecies
+
 # dx_i/dt = sum_{j=0 to n} q_ij*f_j*x_j - phi*x_i = Q.*f*x - phi*x = W*x - phi*x
 # x_i: fraction of infinite population as quasispecies i
 # sum(x) = 1.0
@@ -24,28 +28,4 @@ function quasispecies(x, Q, f)
     return Q*(f.*x) - phi*x
 end
 
-Q = [0.8 0.1 0.1;
-     0.1 0.8 0.1;
-     0.1 0.1 0.8]
-# Q = [0.9 0.05 0.05;
-#      0.05 0.9 0.05;
-#      0.05 0.05 0.9]
-# f = [1.0, 1.0, 1.0]
-# f = [0.5, 0.3, 0.2]
-# f = [1.5, 1.3, 1.2]
-# f = [0.3, 0.5, 0.2]
-f = [1.5, 1.0, 0.5]
-x = [0.1, 0.1, 0.8]
-t = 0.2
-
-printsummary(x)
-
-for i = 1:20
-    xp = quasispecies(x, Q, f)
-    # printsummary(xp)
-    # println("sum(xp): ", sum(xp))
-    x = x + t*(x.*xp)
-    x = x/norm(x, 1)
-    printsummary(x)
-    # println("sum(x): ", sum(x))
 end
