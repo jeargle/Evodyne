3faf2aa1abb938fd2e96515c725128d761c11fc6
John Eargle <jeargle@gmail.com>
2015-08-09 19:23:00 -0500

    run and plot arrays of trajectories

--- test/simPlot.jl
+++ test/simPlot.jl
@@ -20,56 +20,64 @@ Q1 = [0.8 0.1 0.1;
 f1 = [1.5, 1.0, 0.5]
 
 # x must sum to 1
-x1 = [0.1, 0.1, 0.8]
-x2 = [0.1, 0.3, 0.6]
-x3 = [0.1, 0.5, 0.4]
+x1 =  [0.1, 0.1, 0.8]
+x2 =  [0.1, 0.2, 0.7]
+x3 =  [0.1, 0.3, 0.6]
+x4 =  [0.1, 0.4, 0.5]
+x5 =  [0.1, 0.5, 0.4]
+x6 =  [0.1, 0.8, 0.1]
+x7 =  [0.2, 0.7, 0.1]
+x8 =  [0.3, 0.6, 0.1]
+x9 =  [0.4, 0.5, 0.1]
+x10 = [0.5, 0.4, 0.1]
+x11 = [0.8, 0.1, 0.1]
+x12 = [0.7, 0.1, 0.2]
+x13 = [0.6, 0.1, 0.3]
+x14 = [0.5, 0.1, 0.4]
+x15 = [0.4, 0.1, 0.5]
+starts = [x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15]
 
-numsteps = 500
+numsteps = 200
 timestep = 0.2        # timestep
 
 # Run quasispecies simulation
 println("\n*** simulate 1 ***\n")
-traj1 = simulate(x1, Q1, f1, numsteps, timestep)
-traj2 = simulate(x2, Q1, f1, numsteps, timestep)
-traj3 = simulate(x3, Q1, f1, numsteps, timestep)
-print(traj1)
+trajList = Array{Float64,2}[]
+for i = 1:size(starts,2)
+    print(starts[:,i])
+    push!(trajList, simulate(starts[:,i], Q1, f1, numsteps, timestep))
+end
+print(trajList[1])
 
 println("")
-println("*********")
-
-cartCoords1 = zeros(Float64, size(traj1,1), 2)
-cartCoords2 = zeros(Float64, size(traj1,1), 2)
-cartCoords3 = zeros(Float64, size(traj1,1), 2)
-for i = 1:size(traj1,1)
-    println(traj1[i,:])
-    cartCoords1[i,:] = bary2cart(vec(traj1[i,:]))
-    cartCoords2[i,:] = bary2cart(vec(traj2[i,:]))
-    cartCoords3[i,:] = bary2cart(vec(traj3[i,:]))
-end
-
-println("")
-println("*********")
-
-for i = 1:size(cartCoords1,1)
-    println(cartCoords1[i,:])
-end
-
+println("*** cartesian coords ***")
 
 # Get Cartesian coords
+cartCoords = zeros(Float64, size(trajList,1), size(trajList[1],1), 2)
+for i = 1:size(trajList,1)
+    for j = 1:size(trajList[1],1)
+        cartCoords[i,j,:] = bary2cart(vec(trajList[i][j,:]))
+    end
+end
 
-# plot()
+println("")
+println("*** set 1 ***")
+
+for i = 1:size(cartCoords,2)
+    println(vec(cartCoords[1,i,:]))
+end
+
+
 draw(SVG("myplot.svg", 6inch, 6inch),
-     plot(layer(x=cartCoords1[:, 1],
-                y=cartCoords1[:, 2],
-                Geom.line(preserve_order=false)
-                ),
-          layer(x=cartCoords2[:, 1],
-                y=cartCoords2[:, 2],
-                Geom.line(preserve_order=false)
-                ),
-          layer(x=cartCoords3[:, 1],
-                y=cartCoords3[:, 2],
-                Geom.line(preserve_order=false)
+     plot([layer(x=cartCoords[i,:, 1],
+                 y=cartCoords[i,:, 2],
+                 Geom.line(preserve_order=false)
+                 )
+           for i = 1:size(cartCoords,1)]...,
+          layer(x=[0.0, 1.0, 0.5, 0.0],
+                y=[0.0, 0.0, sqrt(3.0)/2, 0.0],
+                Geom.line(preserve_order=false),
+                Theme(default_color=color("black"))
                 ),
           Scale.x_continuous(minvalue=0.0, maxvalue=1.0),
           Scale.y_continuous(minvalue=0.0, maxvalue=1.0)
